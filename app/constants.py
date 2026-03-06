9eadf57f443191cd7a774c26e4e952ad45b5e45f
Subhadip Jana <subhadipjana69@gmail.com>
2026-03-06 14:24:37 +0530

    fix: decoding content of wb

--- app/constants.py
+++ app/constants.py
@@ -3,6 +3,13 @@ IMAGE_EXTENSIONS = {
     ".bmp", ".ico", ".tiff", ".tif",
 }
 
+# Advertise brotli support only if the decoder is actually installed.
+try:
+    import brotli as _brotli  # noqa: F401
+    _ACCEPT_ENCODING = "gzip, deflate, br"
+except ImportError:
+    _ACCEPT_ENCODING = "gzip, deflate"
+
 DEFAULT_HEADERS = {
     "User-Agent": (
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
@@ -11,7 +18,7 @@ DEFAULT_HEADERS = {
     ),
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
     "Accept-Language": "en-US,en;q=0.9",
-    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Encoding": _ACCEPT_ENCODING,
     "Connection": "keep-alive",
     "Upgrade-Insecure-Requests": "1",
     "Sec-Fetch-Dest": "document",
